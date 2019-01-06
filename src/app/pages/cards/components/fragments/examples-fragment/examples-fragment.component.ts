00b1e746850e536bb0f5bbeb90b146d6e6b9cd6c
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-01-06 20:53:51 +0100

    Used automatic translation in examples

--- src/app/pages/cards/components/fragments/examples-fragment/examples-fragment.component.ts
+++ src/app/pages/cards/components/fragments/examples-fragment/examples-fragment.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Vocabel} from '../../../../../core/entity/model/language/vocabel.model';
+import {Language} from '../../../../../core/entity/model/language/language.enum';
+import {MicrosoftTranslateService} from '../../../../../core/translate/services/microsoft-translate.service';
 
 /**
  * Displays an example
@@ -13,12 +15,22 @@ export class ExamplesFragmentComponent implements OnInit {
 
   /** Examples to be displayed */
   @Input() examples: Vocabel[];
+  /** Target language */
+  @Input() targetLanguage: Language;
   /** Placeholder front */
   @Input() placeholderFront = '';
   /** Placeholder back */
   @Input() placeholderBack = '';
   /** Event emitter indicating change in examples */
   @Output() exampleChangedEventEmitter = new EventEmitter<Vocabel[]>();
+
+
+  /**
+   * Constructor
+   * @param microsoftTranslateService Microsoft translate service
+   */
+  constructor(private microsoftTranslateService: MicrosoftTranslateService) {
+  }
 
   //
   // Lifecycle hooks
@@ -50,8 +62,17 @@ export class ExamplesFragmentComponent implements OnInit {
 
   /**
    * Handles example changes
+   * @param example example
    */
-  onExampleChanged() {
+  onExampleSourceChanged(example: Vocabel) {
+    this.translateExample(example, this.targetLanguage);
+    this.exampleChangedEventEmitter.emit(this.examples);
+  }
+
+  /**
+   * Handles example changes
+   */
+  onExampleTargetChanged() {
     this.exampleChangedEventEmitter.emit(this.examples);
   }
 
@@ -73,5 +94,22 @@ export class ExamplesFragmentComponent implements OnInit {
     return this.examples.length > 0 && this.examples.some(example => {
       return example.source.trim() === '' || example.target.trim() === '';
     });
+  }
+
+  // Translation
+
+  /**
+   * Translates a given example
+   * @param example example
+   * @param targetLanguage target language
+   */
+  private translateExample(example: Vocabel, targetLanguage: Language) {
+    const translationEmitter: EventEmitter<string> = new EventEmitter<string>();
+    translationEmitter.subscribe(value => {
+      example.target = value;
+      this.exampleChangedEventEmitter.emit(this.examples);
+    });
+
+    this.microsoftTranslateService.translate(example.source, targetLanguage, translationEmitter);
   }
 }
