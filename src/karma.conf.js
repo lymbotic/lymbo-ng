8c4e6a4eec2679f47da39bcfd66064a96f0daad1
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-09-10 20:43:07 +0200

    Added Jenkinsfile for deployment

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/sciddle'),
+      dir: require('path').join(__dirname, '../coverage/lymbo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
